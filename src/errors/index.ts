162d4590b300712e89846a115689e83684f0cd97
ThiagoCCR <thiago.ccr96@gmail.com>
2023-01-29 11:20:54 -0300

    feat: auth && errors

---
+++ src/errors/index.ts
@@ -0,0 +1,5 @@
+export * from "./unauthorized-error";
+export * from "./invalid-data-error";
+export * from "./duplicated-email-error";
+export * from "./bad-request-error";
+export * from "./invalid-credentials-error";
